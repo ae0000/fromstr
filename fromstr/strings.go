7073288bd5ac2831f8dc28e5481cf78b7298c702
Andrew Edwards <ae0000@gmail.com>
2020-02-01 20:19:11 +1100

    added 'int'

--- fromstr/strings.go
+++ fromstr/strings.go
@@ -12,6 +12,16 @@ func Int64(str string, defaultValue int64) int64 {
 	return i
 }
 
+// Int returns the int out of the string, or if the string is not a
+// valid int, it returns the default
+func Int(str string, defaultValue int) int {
+	i, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return int(i)
+}
+
 // Float64 returns the int64 out of the string, or if the string is not a
 // valid int, it returns the default
 func Float64(str string, defaultValue float64) float64 {
